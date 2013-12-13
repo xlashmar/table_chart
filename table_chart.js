50a797dab26d4afbcc2f0e4017156d16b2342172
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2013-12-13 02:21:21 +0000

    Working prototype with bar graphs

--- table_chart.js
+++ table_chart.js
@@ -1,27 +1,54 @@
 (function($) {
   Drupal.behaviors.table_chart = {
     attach: function(context, settings) {
-      var table_data = $('.view-display-id-dropfort_um_update_status_code_pane table').tableToJSON();
-      console.log(JSON.stringify(table_data));
-      $('.view-display-id-dropfort_um_update_status_code_pane').append('<div id="myfirstchart" style="height: 250px;"></div>');
-      var result = [];
+      var count = 0;
+      $('.table-chart').each(function(){
+        count++;
+        $(this).once(function(){
+          // @todo figure out how to find the table
+          // @todo figure out how to find the table settings
+          var wrapper = $(this);
+          var table = wrapper.find('table');
+          var table_data = table.tableToJSON();
+          var keys = [];
+      
+          // Get the list of keys
+          for (var i in table_data) {
+            for (var j in table_data[i]) {
+              keys.push(j);
+            }
+            break;
+          }
 
-      for(var i in table_data) {
-        result.push([i, table_data [i]]);
-      }
+          table.addClass('element-invisible');
+          wrapper.append('<div id="morris-chart-'+count+'" class="morris-chart" style="height: 250px;"></div><a href="#" class="button toggle-table fa fa-table">' + Drupal.t("Show data") + '</a>');
+      
+          // @todo figure out how to load settings for this stuff.
+          new Morris.Bar({
+            element: 'morris-chart-'+count,
+            data: table_data,
+              // The name of the data record attribute that contains x-values.
+              xkey: keys.shift(),
+              // A list of names of data record attributes that contain y-values.
+              ykeys: keys,
+              // Labels for the ykeys -- will be displayed when you hover over the
+              // chart.
+              labels: keys,
+              hideHover: false,
+              barColors: ['#0B62A4', '#25567B','#043E6B','#3F92D2','#66A3D2']
+          });
 
-          
-      // @todo replace with data from table
-      new Morris.Donut({
-        element: 'myfirstchart',
-        data: result,
-          // The name of the data record attribute that contains x-values.
-//          xkey: 'year',
-          // A list of names of data record attributes that contain y-values.
-//          ykeys: ['value'],
-          // Labels for the ykeys -- will be displayed when you hover over the
-          // chart.
-//          labels: ['Value']
+          wrapper.find('.button.toggle-table').click(function(event){
+            table.toggleClass('element-invisible');
+            if (table.hasClass('element-invisible')) {
+              $(this).text(Drupal.t('Show Data'));
+            }
+            else {
+              $(this).text(Drupal.t('Hide Data'));
+            }
+            event.preventDefault();
+          });
+        });
       });
     }
   };
