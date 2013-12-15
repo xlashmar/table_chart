d80709a38b0c993369e20c5d7d2220bd1afc762c
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2013-12-15 15:57:18 -0500

    Refactored code a bit

--- table_chart.js
+++ table_chart.js
@@ -11,78 +11,48 @@
           var table = wrapper.find('table');
           
           // Chart settings
-          var colors = table.data('morris-colors') ? table.data('morris-colors').split(',') : null;
-          var resize = table.data('morris-resize') ? true == table.data('morris-resize') : false;
-          var type = table.data('morris-type') ? table.data('morris-type') : 'line';
+          var options = {};
+          options.element = 'morris-chart-' + count;
+          options.colors = table.data('morris-colors') ? table.data('morris-colors').split(',') : null;
+          options.resize = table.data('morris-resize') ? true == table.data('morris-resize') : false;
+          options.type = table.data('morris-type') ? table.data('morris-type') : 'line';
 
           // Data settings
-          var ignoreColumns = table.data('tabletojson-ignorecolumns') ? table.data('tabletojson-ignorecolumns').split(',') : [];
-          var onlyColumns = table.data('tabletojson-onlyColumns') ? table.data('tabletojson-onlyColumns').split(',') : null;
-          var ignoreHiddenRows = table.data('tabletojson-ignoreHiddenRows') ? true == table.data('tabletojson-ignoreHiddenRows') : true;
-          var headings = table.data('tabletojson-headings') ? table.data('tabletojson-headings').split(',') : null;
-          var keys = [];
+          options.ignoreColumns = table.data('tabletojson-ignorecolumns') ? table.data('tabletojson-ignorecolumns').split(',') : [];
+          options.onlyColumns = table.data('tabletojson-onlyColumns') ? table.data('tabletojson-onlyColumns').split(',') : null;
+          options.ignoreHiddenRows = table.data('tabletojson-ignoreHiddenRows') ? true == table.data('tabletojson-ignoreHiddenRows') : true;
+          options.headings = table.data('tabletojson-headings') ? table.data('tabletojson-headings').split(',') : null;
+          options.keys = [];
 
           // Data preperation
           
           // Ensure all the values are true integers and not string numbers
-          ignoreColumns = ignoreColumns.map(function (x) { 
+          options.ignoreColumns = options.ignoreColumns.map(function (x) { 
               return parseInt(x, 10); 
           });
           
           // Read the table data with the given configuration
-          var table_data = table.tableToJSON({
-            ignoreColumns: ignoreColumns,
-            onlyColumns: onlyColumns,
-            ignoreHiddenRows: ignoreHiddenRows,
-            headings: headings
+          options.table_data = table.tableToJSON({
+            ignoreColumns: options.ignoreColumns,
+            onlyColumns: options.onlyColumns,
+            ignoreHiddenRows: options.ignoreHiddenRows,
+            headings: options.headings
           });
 
           // Get the list of keys
-          for (var i in table_data) {
-            for (var j in table_data[i]) {
-              keys.push(j);
+          for (var i in options.table_data) {
+            for (var j in options.table_data[i]) {
+              options.keys.push(j);
             }
             break;
           }
 
           // Prepare display
           table.addClass('element-invisible');
-          wrapper.append('<div id="morris-chart-'+count+'" class="morris-chart" style="height: 250px;"></div><a href="#" class="button toggle-table fa fa-table">' + Drupal.t("Show data") + '</a>');
-          var settings = {
-            element: 'morris-chart-'+count,
-            data: table_data,
-            resize: resize
-          };
-
-          switch (type) {
-          case 'bar':
-            if (null !== colors && colors != undefined) {
-              settings.barColors = colors;
-            }
-            settings.xkey = keys.shift();
-            settings.ykeys = keys;
-            settings.labels = keys;
-            settings.hideHover = false;
-            new Morris.Bar(settings);
-            break;
-          case 'donut':
-            if (null !== colors && colors != undefined) {
-              settings.colors = colors;
-            }
-            new Morris.Donut(settings);
-
-            break;
-          case 'area':
-            // @todo adjust area settings
-            new Morris.Area(settings);
-
-            break;
-          case 'line':
-          default:
-            // @todo adjust line settings
-            new Morris.Line(settings);
-          }
-      
+          wrapper.append('<div id="' + options.element + '" class="morris-chart" style="height: 250px;"></div><a href="#" class="button toggle-table fa fa-table">' + Drupal.t("Show data") + '</a>');
+          
+          // Draw the chart with the given options
+          drawChart(options);
 
           wrapper.find('.button.toggle-table').click(function(event){
             table.toggleClass('element-invisible');
@@ -103,6 +73,41 @@
    * Generate the chart with the given defaults
    */
   function drawChart(options) {
-    // @todo move the chart generator into here so we can make the charts redraw live.
+    // Set global settings
+    var settings = {
+      element: options.element,
+      data: options.table_data,
+      resize: options.resize
+    };
+
+    // Set per-type settings
+    switch (type) {
+      case 'bar':
+        if (null !== options.colors && options.colors != undefined) {
+          settings.barColors = options.colors;
+        }
+        settings.xkey = options.keys.shift();
+        settings.ykeys = options.keys;
+        settings.labels = options.keys;
+        settings.hideHover = false;
+        new Morris.Bar(settings);
+        break;
+      case 'donut':
+        if (null !== options.colors && options.colors != undefined) {
+          settings.colors = options.colors;
+        }
+        new Morris.Donut(settings);
+
+        break;
+      case 'area':
+        // @todo adjust area settings
+        new Morris.Area(settings);
+
+        break;
+      case 'line':
+      default:
+        // @todo adjust line settings
+        new Morris.Line(settings);
+    }
   }
 }(jQuery));
